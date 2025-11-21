3f2c1cab1ef42768cc18df5b3c52a0f0f60419da
Christopher Gauthier <WhistlerG99@gmail.com>
2025-11-21 22:45:19 +0000

    getting ready for saudia airlines model

--- preprocessing/preprocess_data_sv.py
+++ preprocessing/preprocess_data_sv.py
@@ -8,6 +8,11 @@ from bid_predictor.preprocessor import (
 )
 
 
+BUCKET = "amazon-sagemaker-622055002283-us-east-1-b37b41a56cd8"
+PREFIX = "dzd_4dt0rvdnr1hoiv/dfbsxtgjets9wn/output"
+OUTPUT_PREFIX = "s3://amazon-sagemaker-622055002283-us-east-1-b37b41a56cd8/dzd_4dt0rvdnr1hoiv/5vt5uv9jpcqmxz/data"
+
+
 avail_cols: List[str] = [
     "available_count_01h",
     "available_count_12h",
@@ -57,16 +62,19 @@ features: List[str] = (
 
 
 if __name__ == "__main__":
-    grp_cols = ["flight_number", "carrier_code", "departure_local_date_time", "cabin_type"]
-    # grp_cols = ["flight_number", "carrier_code", "departure_local_date_time", "upgrade_type"]
+    data_dir = "bid-predictor-historical-by-partners-EY-SV/SV"
+    
+    flights_file = f"s3://{BUCKET}/{PREFIX}/{data_dir}/joined-cols-20251117T202158/"
+    offers_file = f"s3://{BUCKET}/{PREFIX}/{data_dir}/joined-offers-ord-20251117T200423/"
 
-    df_flights = load_flight_data("../data/etihad/raw_data/joined_cols_point3_ey_2years_data.csv")
-    df_flights = df_flights[df_flights.booking_fare_class.isin(["F","J","Y"])]
+    grp_cols = ["flight_number", "carrier_code", "departure_local_date_time", "cabin_type"]
+
+    df_flights = load_flight_data(flights_file)
 
     grps = df_flights.groupby(grp_cols, observed =True).size().sort_values()
     df_flights = df_flights.merge(grps[grps==1].reset_index().drop(columns=0),on=grp_cols)
 
-    df_offers = load_offer_data("../data/etihad/raw_data/point_1_joined_offers_orld_tables_with_usd_col.csv")
+    df_offers = load_offer_data(offers_file)
 
     # extract "FIRST" or "BUSINESS" when they appear as suffixes like "XXX_FIRST" or "XXX_BUSINESS"
     df_offers["upgrade_type"] = (
@@ -77,9 +85,17 @@ if __name__ == "__main__":
     # use None for no-match (consistent with earlier cells)
     df_offers["upgrade_type"] = df_offers["upgrade_type"].where(df_offers["upgrade_type"].notna(), None)
 
+    df_offers = df_offers.rename(columns={
+            "ord_multiplier_fare_class": "multiplier_fare_class",
+            "ord_multiplier_loyalty": "multiplier_loyalty",
+            "ord_multiplier_success_history": "multiplier_success_history",
+            "ord_multiplier_payment_type": "multiplier_payment_type",
+        }
+    )
+
     data = preprocess_data(df_flights, df_offers)
     data["seats_available"] = data.apply(get_seats_available, axis=1)
 
     data = data[features + ["seats_available"]]
 
-    data.to_parquet("../data/etihad/bid_and_flight_data_etihad.parquet", coerce_timestamps="us", allow_truncated_timestamps=True)
+    data.to_parquet(f"{OUTPUT_PREFIX}/saudia/bid_and_flight_data.parquet", coerce_timestamps="us", allow_truncated_timestamps=True)
